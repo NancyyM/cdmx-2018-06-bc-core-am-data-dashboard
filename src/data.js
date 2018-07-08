e28c5e900e5a135c41399f6b46c8b541020233d7
lameme <esme.riveroh@gmail.com>
2018-07-08 12:55:12 -0500

    Se llega hasta topics

--- src/data.js
+++ src/data.js
@@ -54,24 +54,40 @@ window.computeStudentsStats = (laboratoria) => {
 
   for (let key in laboratoria) {
     if (laboratoria.hasOwnProperty(key)) {
-      console.log(key);
+      //console.log(key); Sede
       let generationKey = Object.keys(laboratoria[key].generacion);
-      console.log(generationKey);
+      //console.log(generationKey); Generación
       let generationValue = Object.values(laboratoria[key].generacion);
-      console.log(generationValue);
+      //console.log(generationValue);
       for (let i = 0; i < generationValue.length; i++) {
-        console.log(generationValue[i]);
+        //console.log(generationValue[i]);
         let estudiantes = generationValue[i].estudiantes;
         //console.log(estudiantes);
         for (let j = 0; j < estudiantes.length; j++) {
-          console.log( estudiantes[j]);
+          //console.log( estudiantes[j]);
           let name = estudiantes[j].nombre;
-          console.log(name);
+          console.log(name); //Nombre de cada estudiante
           let email = estudiantes[j].correo;
-          console.log(email);
-          let progress = estudiantes[j].progreso;
-          for (let k = 0; k < progress.length; k++) {
-            console.log(progress[k]);
+          let progressValues = Object.values(estudiantes[j].progreso);
+          //console.log(progressValues);
+          let completedPercentage = progressValues[1];
+          console.log(completedPercentage);
+          let status ="";
+          if (completedPercentage<=60) {
+            status = "Bajo nivel de completitud";
+          }
+          else if(completedPercentage>=90){
+            status = "Nivel alto de completitud";
+          }
+          else {
+            status= "Completitud normal";
+          }
+          console.log(status);
+          let topicsValues = Object.values(estudiantes[j].progreso.temas);
+          console.log(topicsValues);
+
+          for (let k = 0; k < topicsValues.length; k++) {
+            console.log(topicsValues[k].);
           }
         }
       }
